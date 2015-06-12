f860c5d55224884ea269b7cca49a9d4399df574e
nick <nick.zwart@gmail.com>
2015-06-12 09:35:33 -0700

    Fixed gpi build for linux.

--- gpi-framework/build.sh
+++ gpi-framework/build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+# Check the OS b/c of differences in 'cp' (BSD vs GNU).
 
-cp -R bin/ $PREFIX/bin
-cp -R lib/ $PREFIX/lib
-cp -R include/ $PREFIX/include
-cp -R launch/gpi.command $PREFIX/bin/gpi
+# OSX
+if [ "$(uname)" == "Darwin" ]; then
+    cp -R bin/ $PREFIX/bin/
+    cp -R lib/ $PREFIX/lib/
+    cp -R include/ $PREFIX/include/
+fi
 
-cp LICENSE $PREFIX/lib/gpi
-cp COPYING $PREFIX/lib/gpi
-cp COPYING.LESSER $PREFIX/lib/gpi
-cp AUTHORS $PREFIX/lib/gpi
+# Linux
+if [ "$(uname)" == "Linux" ]; then
+    cp -R bin/* $PREFIX/bin/
+    cp -R lib/gpi $PREFIX/lib/
+    cp -R include/* $PREFIX/include/
+fi
+
+# copy and rename 'gpi.command' to 'gpi'
+cp launch/gpi.command $PREFIX/bin/gpi
+
+# copy licenses to lib dir
+cp LICENSE $PREFIX/lib/gpi/
+cp COPYING $PREFIX/lib/gpi/
+cp COPYING.LESSER $PREFIX/lib/gpi/
+cp AUTHORS $PREFIX/lib/gpi/

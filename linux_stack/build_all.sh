416c4116c936dd81879d10a1114f9ff601e80fe3
nick <nick.zwart@gmail.com>
2015-06-11 17:44:19 -0700

    Successful build script for generating a linux stack.
    	- still needs to be wrapped with a makeself.sh script

---
+++ linux_stack/build_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Make sure root is running this script.
+#if [ "$(id -u)" != "0" ]; then
+#    echo "You must be a root user to build." 2>&1
+#    echo "build aborted."
+#    exit 1
+#fi
+
+function buildall ()
+{
+    ./build_stack.sh
+}
+
+time buildall
+
+echo "Done"

d17b13c1f6d91576048f2aabdbeb40fed3092989
nick <nick.zwart@gmail.com>
2015-09-19 12:45:29 -0700

    Cleaned up and commented.

--- gpi-core-nodes/build.sh
+++ gpi-core-nodes/build.sh
@@ -3,17 +3,24 @@
 set -x 
 set -e
 
-mkdir -p ./__build/core
+TGT_NODE_LIB=core
+UNIQUE_DIR=__build # this dir shouldn't exist in the repo
 
-cp LICENSE ./__build/
+# make a unique directory to hold the node-lib for building
+mkdir -p ./$UNIQUE_DIR/$TGT_NODE_LIB
 
+# copy lic files for conda to grab at the end of the build
+cp LICENSE ./$UNIQUE_DIR/
+
+# copy everything to a unique directory b/c c-code is linked based on lib-name
 shopt -s extglob
-mv !(__build) ./__build/core/
+mv !($UNIQUE_DIR) ./$UNIQUE_DIR/$TGT_NODE_LIB/
 
-cd __build
-
+# make the binaries
+cd $UNIQUE_DIR
 which gpi_make
 gpi_make --all --ignore-gpirc -r3
 
+# install under the gpi package
 mkdir -p $PREFIX/lib/gpi/node-libs
-cp -r core $PREFIX/lib/gpi/node-libs/
+cp -r $TGT_NODE_LIB $PREFIX/lib/gpi/node-libs/

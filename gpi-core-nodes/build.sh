f80c6c6fa86971260d3c6de3ec908ccf28c49a42
nick <nick.zwart@gmail.com>
2015-09-19 12:34:55 -0700

    A working build for the nodes.
        -need to use tempfile for the 'core' directory holder
        -need to figure out how conda grabs the LICENSE files and how many
         we can have

--- gpi-core-nodes/build.sh
+++ gpi-core-nodes/build.sh
@@ -1,7 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+set -x 
+set -e
+
+mkdir -p ./__build/core
+
+cp LICENSE ./__build/
+
+shopt -s extglob
+mv !(__build) ./__build/core/
+
+cd __build
 
 which gpi_make
-gpi_make --all
-mkdir -p $PREFIX/lib/gpi/node-libs/core
-cp -R * $PREFIX/lib/gpi/node-libs/core
+gpi_make --all --ignore-gpirc -r3
 
+mkdir -p $PREFIX/lib/gpi/node-libs
+cp -r core $PREFIX/lib/gpi/node-libs/

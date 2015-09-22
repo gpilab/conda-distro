01fed2fa64fcc6dd1339c418d0f4be4c711547b5
ash <aganders3@gmail.com>
2015-09-22 14:06:19 -0700

    Fix recipes to build either py2k or py3k from the same recipe.
            - use "CONDA_PY=27 conda build . --python 2.7" to build py2k
            - use "CONDA_PY=35 conda build . --python 3.5" to build py3k
            - made sure files are copied to the correct location on install
              for gpi and gpi-core-nodes
            - copy all licenses (LICENSE, COPYING, COPYING.LESSER) and
              authors file when installing gpi and gpi-core-nodes

--- gpi-core-nodes/build.sh
+++ gpi-core-nodes/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -x 
-set -e
-
 TGT_NODE_LIB=core
 UNIQUE_DIR=__build # this dir shouldn't exist in the repo
 
@@ -11,6 +8,9 @@ mkdir -p ./$UNIQUE_DIR/$TGT_NODE_LIB
 
 # copy lic files for conda to grab at the end of the build
 cp LICENSE ./$UNIQUE_DIR/
+cp COPYING ./$UNIQUE_DIR/
+cp COPYING.LESSER ./$UNIQUE_DIR/
+cp AUTHORS ./$UNIQUE_DIR/
 
 # copy everything to a unique directory b/c c-code is linked based on lib-name
 shopt -s extglob

5362078690ac53bff7a980b57d7533aed15988b9
ash <aganders3@gmail.com>
2015-09-22 14:17:08 -0700

    Add options to set Python version and toggle uploading in build_all
            - the script should not upload (even if you select 'y') unless
              you have set anaconda-upload: yes in your ~/.condarc

--- build_all.py
+++ build_all.py
@@ -1,7 +1,27 @@
+#!/usr/bin/env python
+from __future__ import print_function, division
+from six.moves import input
 import os, subprocess
 
-for dirname in os.listdir(os.getcwd()):
-    if os.path.isdir(dirname) and not dirname.startswith('.'):
-        conda_build = ['conda', 'build', dirname]
-        subprocess.call(conda_build)
+py_ver = int(input("Enter Python ver as a two-digit number (e.g. 27, or 35): "))
+os.environ['CONDA_PY'] = str(py_ver)
 
+while True:
+    upload_pkgs = input("Upload the packages as they are built ([y]/n)?: ")
+    if upload_pkgs.lower() in ('n', 'no'):
+        upload_pkgs = '--no-anaconda-upload'
+        break
+    elif upload_pkgs.lower() in ('y', 'yes', ''):
+        upload_pkgs = ''
+        break
+    else:
+        print("Invalid input, please enter 'y' or 'n'.")
+
+for dirname in ('astyle', 'fftw', 'eigen', 'gpi-framework', 'gpi-core-nodes'):
+    print(dirname)
+    if os.path.isdir(dirname) and not dirname.startswith('.'):
+        conda_build = ['conda', 'build', dirname,
+                       '--python {}'.format(py_ver/10.), upload_pkgs]
+        build_command = ' '.join(conda_build)
+        print(build_command)
+        print(subprocess.call(build_command, shell=True))

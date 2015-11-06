d522f5b8b412629e256d56ce445f6a5819c5ba85
nick <nick.zwart@gmail.com>
2015-11-06 13:21:45 -0700

    Fixed some urls, version numbers and added simple package scripts.

---
+++ gpi-core-nodes/package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+USER=gpi
+CHANNEL=main
+CONDA_PY=35
+PKG_BUILDNUM=0
+
+export CONDA_PY
+export PKG_BUILDNUM
+
+OUTPUT=`conda build ./ --output`
+echo "OUTPUT" $OUTPUT
+conda build ./ -c $USER
+anaconda upload -u $USER -c $CHANNEL $OUTPUT --force

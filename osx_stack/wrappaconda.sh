15b2552d7fe074b7a281b1c02f000126d7c1ddcc
nick <nick.zwart@gmail.com>
2015-11-10 20:15:59 -0700

    Spiffed up packaging script to be more readable.

--- osx_stack/wrappaconda.sh
+++ osx_stack/wrappaconda.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-NAME=$1
+NAME=$1 # could be GPI or GPI_TEST, etc...
+TARGET_BINARY=gpi.app
 GPI_VERSION="1.0.0-rc"
+CHANNEL=https://conda.anaconda.org/gpi/channel/rc
+PACKAGES="gpi,gpi-core-nodes,gpi-docs"
+PYTHON_VERSION=3.5
+APP_ICON=gpi.icns
+
 WRAPPACONDA=wrappaconda
-$WRAPPACONDA -n $NAME -t gpi.app -v $GPI_VERSION -i gpi.icns -c https://conda.anaconda.org/gpi/channel/rc -p gpi,gpi-core-nodes,gpi-docs -o
+$WRAPPACONDA -n $NAME -t $TARGET_BINARY -v $GPI_VERSION -i $APP_ICON \
+             -c $CHANNEL -p $PACKAGES -o --py $PYTHON_VERSION

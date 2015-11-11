aee614b16e1bc38111671040d3078c15b59607eb
nick <nick.zwart@gmail.com>
2015-11-11 13:06:16 -0700

    Added full command to finished statement.

--- build_all.py
+++ build_all.py
@@ -170,4 +170,4 @@ for dirname in packages:
             if not a.dry_run:
                 subprocess.call(upload_command, shell=True)
 
-print("Finished "+sys.argv[0])
+print("Finished "+' '.join(sys.argv))

334f2841b4b3448963d3052cde4a073b9a9c51c4
nick <nick.zwart@gmail.com>
2015-11-11 14:50:55 -0700

    Fixed packaged docs path.

--- gpi-docs/build.sh
+++ gpi-docs/build.sh
@@ -3,6 +3,7 @@
 set -x 
 set -e
 
-# install under the gpi package
-mkdir -p $PREFIX/lib/gpi/doc
-cp -r * $TGT_NODE_LIB $PREFIX/lib/gpi/doc/
+DOCS_DIR=$PREFIX/share/doc/gpi
+
+mkdir -p $DOCS_DIR
+cp -r * ${DOCS_DIR}/

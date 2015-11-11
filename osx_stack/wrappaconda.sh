370da926e16a3840d14e1deee68bee912463ca64
nick <nick.zwart@gmail.com>
2015-11-11 16:57:57 -0700

    Added better version placeholder.

--- osx_stack/wrappaconda.sh
+++ osx_stack/wrappaconda.sh
@@ -3,7 +3,11 @@
 # APP/Bundle
 NAME=$1 # could be GPI or GPI_TEST, etc... results in <NAME>.app
 TARGET_BINARY=gpi.app # what the bundle.app runs
-GPI_VERSION="dev" # for the bundle.app (not sure where it gets reported)
+# GPI_VERSION is for the bundle.app (reported in Finder.app)
+# Its set to 'check about page' since the app shell doesn't get replaced in
+# updates. -it might be possible to get a new info.plist from conda.
+GPI_VERSION="Check_About_Page" 
+
 
 # Conda
 CHANNEL=https://conda.anaconda.org/gpi/channel/rc

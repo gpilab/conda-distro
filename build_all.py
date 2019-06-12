0e131d6631944cbe31e59aee19ffe9691fbb1917
Ashley Anderson <aganders3@gmail.com>
2019-06-12 12:24:45 -0500

    Add build variants for core nodes

--- build_all.py
+++ build_all.py
@@ -48,6 +48,10 @@ def add_args(parser):
                         help="Choose the version of python: 26, 27, 34, 35, 36. The default is 36.",
                         default=36)
 
+    parser.add_argument("--numpy-version", "-np", type=int,
+                        help="Choose the version of numpy: 26, 27, 34, 35, 36. The default is 36.",
+                        default=36)
+
     parser.add_argument("--build-number", "-b", type=int,
                         help="Speficy a build number added to the build string e.g. py35_<buildnumber>.",
                         default=0)
@@ -60,86 +64,91 @@ if __name__ == "__main__":
     add_args(parser)
     args = parser.parse_args()
 
-    if args.python_version in [26,27,34,35,36]:
-        os.environ['CONDA_PY'] = str(args.python_version)
+    if args.python_version in [26,27,34,35,36,37]:
+        # os.environ['CONDA_PY'] = str(args.python_version)
+        pass
     else:
         print(f"Invalid python version selected ({args.python_version}), aborting.")
         sys.exit(1)
-    
+
     os.environ['PKG_BUILDNUM'] = str(args.build_number)
 
-    os.environ['PKG_BUILD_STRING'] = f"py{os.environ['CONDA_PY']}_{os.environ['PKG_BUILDNUM']}"
-    
-    if args.release_candidate:
-        os.environ['RELEASE_STRING'] = '_rc'
-        os.environ['PKG_BUILD_STRING'] += os.environ['RELEASE_STRING']
+    # os.environ['PKG_BUILD_STRING'] = f"py{os.environ['CONDA_PY']}_{os.environ['PKG_BUILDNUM']}"
+
+    # if args.release_candidate:
+    #     os.environ['RELEASE_STRING'] = '_rc'
+    #     os.environ['PKG_BUILD_STRING'] += os.environ['RELEASE_STRING']
 
     def validPackage(name):
         return name in packages
-    
+
     # if the user has chosen a specific package(s) to build
     # validate all chosen packages
     for pkg in args.package:
         if not validPackage(pkg):
             print(args.package, ' is not a valid package name, abort.')
             sys.exit(1)
-    
+
     # pass all chosen packages
     packages = args.package
-    
+
     # build all packages in the list
     for dirname in packages:
         if os.path.isdir(dirname) and not dirname.startswith('.'):
-    
+
             ## ASSEMBLE COMMANDS
             # BUILD COMMAND
-            conda_build = ['conda', 'build', dirname,
-                           f"--python {int(os.environ['CONDA_PY'])/10}"]
+            conda_build = ['conda', 'build', dirname]
+            # conda_build += [f"--python {int(os.environ['CONDA_PY'])/10}"]
             conda_build += ['--no-anaconda-upload']
+            conda_build += ['--override-channels']
 
             for ch in args.channel:
                 conda_build.append(f"-c {ch}")
             build_command = ' '.join(conda_build)
-            pkgname = subprocess.Popen(build_command + ' --output', shell=True,
+            pkgnames = subprocess.Popen(build_command + ' --output', shell=True,
                                        stdout=subprocess.PIPE).stdout.read().strip()
-    
+
             # split out the upload from the build so we have more control
-            pkgname = pkgname.decode('ascii') # required by py3
+            pkgnames = pkgnames.decode('ascii') # required by py3
 
-            # UPLOAD COMMAND
-            anaconda_upload = ['anaconda', 'upload', pkgname]
-    
-            if args.skip_built:
-                if os.path.isfile(pkgname):
-                    print('\t', dirname, ' is already built, skipping...')
-                    continue
-    
-            # build deps will require the gpi channel to be in the env
-            anaconda_upload.append(f"-u {args.upload_channel}")
-            for tag in args.upload_tag:
-                anaconda_upload.append(f"-l {tag}")
-            if args.release_candidate:
-                anaconda_upload.append('-l rc')
-            if args.force_upload:
-                anaconda_upload.append('--force')
+            pkgnames = pkgnames.split()
 
-            upload_command = ' '.join(anaconda_upload)
-    
+            upload_commands = []
+            for pkg in pkgnames:
+                if args.skip_built:
+                    if os.path.isfile(pkg):
+                        print('\t', dirname, ' is already built, skipping...')
+                        continue
+
+                anaconda_upload = ['anaconda', 'upload', pkg]
+
+                # build deps will require the gpi channel to be in the env
+                anaconda_upload.append(f"-u {args.upload_channel}")
+                for tag in args.upload_tag:
+                    anaconda_upload.append(f"-l {tag}")
+                if args.release_candidate:
+                    anaconda_upload.append('-l rc')
+                if args.force_upload:
+                    anaconda_upload.append('--force')
+
+                upload_commands.append(' '.join(anaconda_upload))
+
             ## EXECUTE COMMANDS
             # BUILD
             print(build_command)
             if not args.dry_run:
                 subprocess.call(build_command, shell=True)
-    
-                if not os.path.isfile(pkgname):
-                    print('\n\nBuild FAILED for package: ', dirname)
-                    print('\tThe following file doesn\'t exist: ', pkgname, '\n\n')
-                    sys.exit(1)
-    
+
+                for pkg in pkgnames:
+                    if not os.path.isfile(pkg):
+                        print('\n\nBuild FAILED for package: ', dirname)
+                        print('\tThe following file doesn\'t exist: ', pkg, '\n\n')
+                        continue
+
             # UPLOAD
-            if args.auto_upload:
-                print(upload_command)
-                if not args.dry_run:
-                    subprocess.call(upload_command, shell=True)
-    
+            if args.auto_upload and not args.dry_run:
+                for upload in upload_commands:
+                    subprocess.call(upload, shell=True)
+
     print("Finished "+' '.join(sys.argv))
--- gpi-core-nodes/build.sh
+++ gpi-core-nodes/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 TGT_NODE_LIB=core
 UNIQUE_DIR=__build # this dir shouldn't exist in the repo
 
@@ -7,10 +6,10 @@ UNIQUE_DIR=__build # this dir shouldn't exist in the repo
 mkdir -p ./$UNIQUE_DIR/$TGT_NODE_LIB
 
 # copy lic files for conda to grab at the end of the build
-cp LICENSE ./$UNIQUE_DIR/
-cp COPYING ./$UNIQUE_DIR/
-cp COPYING.LESSER ./$UNIQUE_DIR/
-cp AUTHORS ./$UNIQUE_DIR/
+# cp LICENSE ./$UNIQUE_DIR/
+# cp COPYING ./$UNIQUE_DIR/
+# cp COPYING.LESSER ./$UNIQUE_DIR/
+# cp AUTHORS ./$UNIQUE_DIR/
 
 # copy everything to a unique directory b/c c-code is linked based on lib-name
 shopt -s extglob
@@ -18,8 +17,8 @@ mv !($UNIQUE_DIR) ./$UNIQUE_DIR/$TGT_NODE_LIB/
 
 # make the binaries
 cd $UNIQUE_DIR
-which gpi_make
-gpi_make --all --ignore-gpirc -r3
+# rm core/spiral/spiral_PyMOD.cpp
+gpi_make --all --ignore-gpirc -r 3
 
 # install under the gpi package
 mkdir -p $SP_DIR/gpi/node-libs

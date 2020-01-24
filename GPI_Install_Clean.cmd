eeb589614a123937c2157bad70720e57763216db
Daniel Borup <borup.daniel@mayo.edu>
2020-01-24 11:25:03 -0600

    Set up Windows installer to use default path ~/gpi_stack

--- GPI_Install_Clean.cmd
+++ GPI_Install_Clean.cmd
@@ -1,9 +1,6 @@
 @echo off
 setlocal
 
-if [%1]==[] goto help
-if "%1"=="\H" goto help
-
 :: check for available commands
 WHERE curl >nul 2>nul
 if %ERRORLEVEL% neq 0 (
@@ -12,22 +9,29 @@ if %ERRORLEVEL% neq 0 (
   goto :eof
 )
 
+if "%1"=="\H" goto help
+
 :: conda install location
-set MINICONDA_PATH=%~f1
-if not %MINICONDA_PATH:~-1%==\ SET MINICONDA_PATH=%MINICONDA_PATH%\
+if [%1]==[] (
+  set MINICONDA_PATH=%userprofile%\gpi_stack\
+) else (
+  set MINICONDA_PATH=%~f1
+  if not %MINICONDA_PATH:~-1%==\ SET MINICONDA_PATH=%MINICONDA_PATH%\
+)
 set CONDA=%MINICONDA_PATH%Scripts\conda.exe
 for %%d in (%MINICONDA_PATH%..) do set PATHTOTHEPATH=%%~fd
 
 if not exist %PATHTOTHEPATH% (
-  @echo The parent path %PATHTOTHEPATH% doesn't exist.
+  @echo The desired installation folder, %PATHTOTHEPATH%, doesn't exist.
   goto :eof
 )
 :: See if the directory is already in use
 if exist %MINICONDA_PATH% (
-  @echo "The supplied directory already exists, installation aborted."
+  @echo The installation directory %PATHTOTHEPATH% already exists, installation aborted.
+  @echo Installation aborted.
   goto :eof
 )
-@echo Installing the GPI stack for python %PYTHON_VER% ...
+@echo This script will install GPI into %MINICONDA_PATH%
 :: Install MiniConda
 cd %PATHTOTHEPATH%
 set MINICONDA_WEB=https://repo.continuum.io/miniconda
@@ -41,15 +45,9 @@ mkdir %TMPDIR%
 cd %TMPDIR%
 
 :: Run install script
-@echo.
-@echo "Downloading MiniConda..."
 curl %MINICONDA_WEB%/%MINICONDA_SCRIPT% --output %MINICONDA_SCRIPT%
-@echo.
-@echo Installing MiniConda...
 %MINICONDA_SCRIPT% /AddToPath=0 /S /D=%MINICONDA_PATH%
 
-@echo.
-@echo Installing GPI and the gpi_core nodes...
 set PATH=%MINICONDA_PATH%\bin;%PATH%
 set PATH=%MINICONDA_PATH%\Scripts;%PATH%
 set PATH=%MINICONDA_PATH%\Library\bin;%PATH%
@@ -61,11 +59,10 @@ set PATH=%MINICONDA_PATH%;%PATH%
 %CONDA% create -y -n gpi
 %CONDA% install -y -n gpi gpi_core python=3.7 pyqt=5.9
 
-@echo "Removing package files..."
+@echo "Cleaning Up..."
 %CONDA% clean -tiply
 
 :: Clean up the downloaded files
-@echo Removing temporary files...
 cd %PATHTOTHEPATH%
 RMDIR /S /Q %TMPDIR%
 
